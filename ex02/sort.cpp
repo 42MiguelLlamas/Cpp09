81b7e0e18a8a00b657eb8f7bb51a77d99540adc3
Miguel Llamas Saez <mllamas-@c2r12s4.42madrid.com>
2025-01-18 12:12:20 +0100

    Haciendo el insert

--- ex02/sort.cpp
+++ ex02/sort.cpp
@@ -17,25 +17,25 @@ int powerOfTwo(int x)
     return result;
 }
 
-void merge(std::deque<int>& dq, unsigned long order)
+void merge(std::deque<int>& dq, unsigned long *order)
 {
-	std::cout << "Order is: "<< order << std::endl;
-    if (order <= (dq.size() / 2))
+	std::cout << "Order is: "<< *order << std::endl;
+    if (*order <= (dq.size() / 2))
 	{
 		std::cout << "Merge."<< std::endl;
-        for (unsigned long i = 0; i < (dq.size() / order); i++)
+        for (unsigned long i = 0; i < (dq.size() / *order); i++)
 		{
-			unsigned long index1 = order - 1 + (order * 2 * i);
-            unsigned long index2 = order * 2 - 1 + (order * 2 * i);
+			unsigned long index1 = *order - 1 + (*order * 2 * i);
+            unsigned long index2 = *order * 2 - 1 + (*order * 2 * i);
 			if (i == 0)
 			{
-				index1 = order - 1;
-            	index2 = order * 2 - 1;
+				index1 = *order - 1;
+            	index2 = *order * 2 - 1;
 			}
 			std::cout << "Index 1: " << index1 << " | " << "Index 2: " << index2 << std::endl;
             if (index1 >= 0 && index2 < dq.size())
 			{
-				unsigned long pack_size = order;
+				unsigned long pack_size = *order;
                 unsigned long start1 = index1 - (pack_size - 1);
                 unsigned long start2 = index2 - (pack_size - 1);
                 if (dq[index1] > dq[index2])
@@ -57,14 +57,40 @@ void merge(std::deque<int>& dq, unsigned long order)
 			std::cout << *it << " ";
 		}
 		std::cout << std::endl;
-        merge(dq, order * 2);
+		*order = *order * 2;
+        merge(dq, order);
+    }
+}
+
+void insert(std::deque<int>& dq, unsigned long *order)
+{
+	//AQUI METER LOGICA QUE INSERTE EL BLOQUE EN LA POSICION QUE LE CORRESPONDA
+
+    unsigned long start_pos = dq.size() - *order; // Última posición del bloque desordenado.
+    unsigned long i = dq.size() - *order - 1; // Recorrer desde el principio del bloque desordenado.
+
+    // Buscar la posición correcta para insertar el bloque.
+    while (i >= 0 && dq[i] > dq[start_pos]) {
+        --i;
+    }
+
+    // Mover los elementos a la derecha para hacer espacio
+    for (unsigned long j = dq.size() - 1; j > i; --j) {
+        dq[j] = dq[j - 1];
+    }
+
+    // Insertar el bloque en la posición encontrada
+    for (unsigned long k = 0; k < *order; ++k) {
+        dq[i + 1 + k] = dq[start_pos + k];
     }
 }
 
 void sort(std::deque<int> dq)
 {
-	merge(dq, 1);
-	//insert(dq);
+	unsigned long order = 1;
+	merge(dq, &order);
+	order = order / 2;
+	//insert(dq, &order);
 }
 
 int main(void)
